acb4fba6333e5f5f81b58b3f2d0a2357d50b0e48
Ochanda <lewiscrafton95@gmail.com>
2018-08-30 13:56:09 +0300

    Commit

---
+++ app/src/main/java/com/montylee/victor/FireIDService.java
@@ -0,0 +1,10 @@
+package com.montylee.victor;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+
+    }
+}
